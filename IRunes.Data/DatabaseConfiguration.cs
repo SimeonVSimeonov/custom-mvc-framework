18d8bdb59d1731996b5eba6910f189517f48ec43
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-05-29 16:46:18 +0300

    first commit

---
+++ IRunes.Data/DatabaseConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.Data
+{
+    public class DatabaseConfiguration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-KDM5RU8\SQLEXPRESS;Database=IRunesApp;Integrated Security=True;";
+    }
+}
